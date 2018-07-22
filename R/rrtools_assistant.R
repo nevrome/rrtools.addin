4ca24e3149a5ca95f9a951490951814687a3ef46
nevrome <clemens@nevrome.de>
2018-07-22 22:17:15 +0200

    filled overview page with some example content

--- R/rrtools_assistant.R
+++ R/rrtools_assistant.R
@@ -3,6 +3,7 @@
 #' @export
 rrtools_assistant <- function() {
 
+  #### ui ####
   ui <- function() {
     miniUI::miniPage(
       shiny::tags$head(
@@ -12,11 +13,31 @@ rrtools_assistant <- function() {
         "rrtools Configuration Assistant",
         right = NULL
       ),
+      # overview page
       miniUI::miniTabstripPanel(
         miniUI::miniTabPanel(
           title = "Overview",
-          icon = shiny::icon("question")
+          icon = shiny::icon("question"),
+          shiny::fillRow(
+            flex = c(1, 2),
+            shiny::fillCol(
+              shiny::div(
+                class = "helpbox",
+                shiny::h4("What's this addin about"),
+                shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+              )
+            ),
+            shiny::fillCol(
+              shiny::div(
+                class = "imagebox",
+                shiny::img(
+                  src = "https://i.pinimg.com/originals/39/a3/d5/39a3d528f9c54ad894d1a027bf69e43d.jpg"),
+                  align = "center"
+              )
+            )
+          )
         ),
+        # other ui elements
         setup_ui(),
         license_ui("license_general"),
         versioning_ui(),
@@ -30,6 +51,7 @@ rrtools_assistant <- function() {
     )
   }
 
+  #### server ####
   server <- function(input, output, session) {
 
     shiny::callModule(license_server, id = "license_general")
@@ -40,6 +62,7 @@ rrtools_assistant <- function() {
 
   }
 
+  #### run app ####
   shiny::runGadget(
     ui,
     server,
