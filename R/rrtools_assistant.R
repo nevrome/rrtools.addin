1080bb9b3fa7ec7753f7bbcfb3c035cd3584c13d
nevrome <clemens@nevrome.de>
2018-07-22 21:40:24 +0200

    reverted more complicated than useful outsourcing

--- R/rrtools_assistant.R
+++ R/rrtools_assistant.R
@@ -3,7 +3,32 @@
 #' @export
 rrtools_assistant <- function() {
 
-  ui <- rrtools_assistant_ui()
+  ui <- function() {
+    miniUI::miniPage(
+      shiny::tags$head(
+        shiny::includeCSS(system.file("style/rrtools_assistant_stylesheet.css", package = "rrtools.addin"))
+      ),
+      miniUI::gadgetTitleBar(
+        "rrtools Configuration Assistant",
+        right = NULL
+      ),
+      miniUI::miniTabstripPanel(
+        miniUI::miniTabPanel(
+          title = "Overview",
+          icon = shiny::icon("question")
+        ),
+        setup_ui(),
+        license_ui("license_general"),
+        versioning_ui(),
+        cloud_ui(),
+        readme_ui(),
+        file_structure_ui(),
+        virtualisation_ui(),
+        ci_cd_ui(),
+        tests_ui()
+      )
+    )
+  }
 
   server <- function(input, output, session) {
 
