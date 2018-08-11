f2511d4106486404cbc72731683a7ec0992ecf9b
nevrome <clemens@nevrome.de>
2018-08-11 15:51:44 +0200

    improved interface

--- R/readme_ui.R
+++ R/readme_ui.R
@@ -10,13 +10,27 @@ readme_ui <- function(id) {
 		  shiny::fillCol(
 		    shiny::div(
 		      class = "explainbox_1",
-		      shiny::h4("A nice R1ADME?"),
-		      shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+		      shiny::h4("A nice and useful README?"),
+		      shiny::HTML("<p> 
+		        Especially if you share your work on cloud version control platforms
+            like Github the README file is the 
+            <a href=\"https://help.github.com/articles/about-readmes/\" rel=\"nofollow\">face of your project</a>
+            for everybody who comes across it. It's important to give a good 
+            overview about what the project does and how users can get started with it.
+		      </p>")
 		    ),
 		    shiny::div(
 		      class = "explainbox_2",
 		      shiny::h4("The function", shiny::strong("rrtools::use_readme_rmd")),
-		      shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+		      shiny::HTML("            
+            <ol>
+              <li>Creating default README.Rmd</li>
+              <li>Rendering README.Rmd to Readme.md</li>
+              <li>Creating code of conduct</li>
+              <li>Creating instructions to contributors</li>
+              <li>Adding additional files to .Rbuildignore</li>
+            </ol>
+		      ")
 		    )
 		  ),
 		  shiny::fillCol(
