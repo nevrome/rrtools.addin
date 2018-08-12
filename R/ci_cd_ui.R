944c03a0e74e81df152d5194b3efa4fa2ade4edc
nevrome <clemens@nevrome.de>
2018-08-12 21:59:35 +0200

    begin of the work on the remaining sections

--- R/ci_cd_ui.R
+++ R/ci_cd_ui.R
@@ -10,13 +10,27 @@ ci_cd_ui <- function(id) {
       shiny::fillCol(
         shiny::div(
           class = "explainbox_1",
-          shiny::h4("Continous Integration?"),
-          shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+          shiny::h4("Continous integration?"),
+          shiny::HTML("<p> 
+		        Especially if you share your work on cloud version control platforms
+            like Github the README file is the 
+            <a href=\"https://help.github.com/articles/about-readmes/\" rel=\"nofollow\">face of your project</a>
+            for everybody who comes across it. It's important to give a good 
+            overview about what the project does and how users can get started with it.
+		      </p>")
         ),
         shiny::div(
           class = "explainbox_2",
-          shiny::h4("The function", shiny::strong("rrtools::use_travis")),
-          shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+          shiny::h4("The function", shiny::strong("rrtools::use_analysis")),
+          shiny::HTML("
+		        <ol>
+              <li>Creating default README.Rmd</li>
+              <li>Rendering README.Rmd to Readme.md</li>
+              <li>Creating code of conduct</li>
+              <li>Creating instructions to contributors</li>
+              <li>Adding additional files to .Rbuildignore</li>
+            </ol>
+		      ")
         )
       ),
       shiny::fillCol(
@@ -24,7 +38,7 @@ ci_cd_ui <- function(id) {
           class = "function_interface_outer",
           shiny::div(
             class = "function_interface_inner",
-            shiny::h1("Travis"),
+            shiny::h1("Continous integration with Travis CI"),
             shiny::selectInput(
               inputId = ns("docker_selection"),
               label = "With Docker?",
--- R/file_structure_ui.R
+++ R/file_structure_ui.R
@@ -10,13 +10,27 @@ file_structure_ui <- function(id) {
       shiny::fillCol(
         shiny::div(
           class = "explainbox_1",
-          shiny::h4("Bringing order into my files?"),
-          shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+          shiny::h4("Writing a paper in R?"),
+          shiny::HTML("<p> 
+		        The most important part of a research compendium is the actual analysis document. 
+            There are powerful tools for text production in R and the 
+            <a href=\"https://bookdown.org/\" rel=\"nofollow\">bookdown framework</a>
+            combines them to provide a writing environment, 
+            that still can produce output in PDF, HTML and even Word.
+		      </p>")
         ),
         shiny::div(
           class = "explainbox_2",
           shiny::h4("The function", shiny::strong("rrtools::use_analysis")),
-          shiny::p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh. Libero id faucibus nisl tincidunt eget. Praesent semper feugiat nibh sed.")
+          shiny::HTML("
+		        <ol>
+              <li>Creating default README.Rmd</li>
+              <li>Rendering README.Rmd to Readme.md</li>
+              <li>Creating code of conduct</li>
+              <li>Creating instructions to contributors</li>
+              <li>Adding additional files to .Rbuildignore</li>
+            </ol>
+		      ")
         )
       ),
       shiny::fillCol(
